26c4cebbabadb032943424b8342c6e2080bc7bd8
M.Faqih Ridho <74019017+MFaqihRidho@users.noreply.github.com>
2022-01-04 08:51:10 +0700

    update country list using react router

--- src/components/country list/index.js
+++ src/components/country list/index.js
@@ -1,189 +1,273 @@
+import { useNavigate } from 'react-router-dom';
+import Button from '../../components/atoms/button'
 import './style.css'
 
 function Country(){
+    let navigate = useNavigate();
+    const clickHandle = ((e) => {
+        navigate(`/list/${e.target.id}`)
+    })
     return(
         <div id='browse' className="country-list container">
-            <a href='/list/American' >
-                <img src="https://flagcdn.com/us.svg"
+            <Button title={
+                <div>
+                    <img id='American' src="https://flagcdn.com/us.svg"
                     width="80"
                     alt="American" />
-                <p>American</p>
-            </a>
+                    <p id='American'>American</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='American' >
+            </Button>
 
-            <a href="/list/British">
-                <img src="https://flagcdn.com/gb.svg"
+            <Button title={
+                <div>
+                    <img id='British' src="https://flagcdn.com/gb.svg"
                     width="80"
                     alt="British" />
-                <p>British</p>
-            </a>
+                    <p id='British'>British</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='British' >
+            </Button>
 
-            <a href="/list/Canadian">
-                <img src="https://flagcdn.com/ca.svg"
+            <Button title={
+                <div>
+                    <img id='Canadian' src="https://flagcdn.com/ca.svg"
                     width="80"
                     alt="Canadian" />
-                <p>Canadian</p>
-            </a>
+                    <p id='Canadian'>Canadian</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Canadian' >
+            </Button>
 
-            <a href="/list/Chinese">
-                <img src="https://flagcdn.com/cn.svg"
+            <Button title={
+                <div>
+                    <img id='Chinese' src="https://flagcdn.com/cn.svg"
                     width="80"
                     alt="Chinese" />
-                <p>Chinese</p>
-            </a>
+                    <p id='Chinese'>Chinese</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Chinese' >
+            </Button>
 
-            <a href="/list/Croatian">
-                <img src="https://flagcdn.com/hr.svg"
+            <Button title={
+                <div>
+                    <img id='Croatian' src="https://flagcdn.com/hr.svg"
                     width="80"
                     alt="Croatian" />
-                <p>Croatian</p>
-            </a>
-
-            <a href="/list/Dutch">
-                <img src="https://flagcdn.com/nl.svg"
+                    <p id='Croatian'>Croatian</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Croatian' >
+            </Button>
+            
+            <Button title={
+                <div>
+                    <img id='Dutch' src="https://flagcdn.com/nl.svg"
                     width="80"
                     alt="Dutch" />
-                <p>Dutch</p>
-            </a>
-            
-            <a href="/list/Egyptian">
-                <img src="https://flagcdn.com/eg.svg"
+                    <p id='Dutch'>Dutch</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Dutch' >
+            </Button>
+
+            <Button title={
+                <div>
+                    <img id='Egyptian' src="https://flagcdn.com/eg.svg"
                     width="80"
                     alt="Egyptian" />
-                <p>Egyptian</p>
-            </a>
+                    <p id='Egyptian'>Egyptian</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Egyptian' >
+            </Button>
 
-            <a href="/list/French">
-                <img src="https://flagcdn.com/fr.svg"
+            <Button title={
+                <div>
+                    <img id='French' src="https://flagcdn.com/fr.svg"
                     width="80"
                     alt="French" />
-                <p>French</p>
-            </a>
+                    <p id='French'>French</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='French' >
+            </Button>
 
-            <a href="/list/Greek">
-                <img src="https://flagcdn.com/gr.svg"
+            <Button title={
+                <div>
+                    <img id='Greek' src="https://flagcdn.com/gr.svg"
                     width="80"
                     alt="Greek" />
-                <p>Greek</p>
-            </a>
+                    <p id='Greek'>Greek</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Greek' >
+            </Button>
 
-            <a href="/list/Indian">
-                <img src="https://flagcdn.com/in.svg"
+            <Button title={
+                <div>
+                    <img id='Indian' src="https://flagcdn.com/in.svg"
                     width="80"
                     alt="Indian" />
-                <p>Indian</p>
-            </a>
+                    <p id='Indian'>Indian</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Indian' >
+            </Button>
 
-            <a href="/list/Irish">
-                <img src="https://flagcdn.com/ie.svg"
+            <Button title={
+                <div>
+                    <img id='Irish' src="https://flagcdn.com/ie.svg"
                     width="80"
                     alt="Irish" />
-                <p>Irish</p>
-            </a>
+                    <p id='Irish'>Irish</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Irish' >
+            </Button>
 
-            <a href="/list/Italian">
-                <img src="https://flagcdn.com/it.svg"
+            <Button title={
+                <div>
+                    <img id='Italian' src="https://flagcdn.com/it.svg"
                     width="80"
                     alt="Italian" />
-                <p>Italian</p>
-            </a>
+                    <p id='Italian'>Italian</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Italian' >
+            </Button>
 
-            <a href="/list/Jamaican">
-                <img src="https://flagcdn.com/jm.svg"
+            <Button title={
+                <div>
+                    <img id='Jamaican' src="https://flagcdn.com/jm.svg"
                     width="80"
                     alt="Jamaican" />
-                <p>Jamaican</p>
-            </a>
+                    <p id='Jamaican'>Jamaican</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Jamaican' >
+            </Button>
 
-            <a href="/list/Japanese">
-                <img src="https://flagcdn.com/jp.svg"
+            <Button title={
+                <div>
+                    <img id='Japanese' src="https://flagcdn.com/jp.svg"
                     width="80"
                     alt="Japanese" />
-                <p>Japanese</p>
-            </a>
+                    <p id='Japanese'>Japanese</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Japanese' >
+            </Button>
 
-            <a href="/list/Kenyan">
-                <img src="https://flagcdn.com/ke.svg"
+            <Button title={
+                <div>
+                    <img id='Kenyan' src="https://flagcdn.com/ke.svg"
                     width="80"
                     alt="Kenyan" />
-                <p>Kenyan</p>
-            </a>
+                    <p id='Kenyan'>Kenyan</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Kenyan' >
+            </Button>
 
-            <a href="/list/Malaysian">
-                <img src="https://flagcdn.com/my.svg"
+            <Button title={
+                <div>
+                    <img id='Malaysian' src="https://flagcdn.com/my.svg"
                     width="80"
                     alt="Malaysian" />
-                <p>Malaysian</p>
-            </a>
+                    <p id='Malaysian'>Malaysian</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Malaysian' >
+            </Button>
 
-            <a href="/list/Mexican">
-                <img src="https://flagcdn.com/mx.svg"
+            <Button title={
+                <div>
+                    <img id='Mexican' src="https://flagcdn.com/mx.svg"
                     width="80"
                     alt="Mexican" />
-                <p>Mexican</p>
-            </a>
+                    <p id='Mexican'>Mexican</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Mexican' >
+            </Button>
 
-            <a href="/list/Moroccan">
-                <img src="https://flagcdn.com/ma.svg"
+            <Button title={
+                <div>
+                    <img id='Moroccan' src="https://flagcdn.com/ma.svg"
                     width="80"
                     alt="Moroccan" />
-                <p>Moroccan</p>
-            </a>
+                    <p id='Moroccan'>Moroccan</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Moroccan' >
+            </Button>
 
-            <a href="/list/Polish">
-                <img src="https://flagcdn.com/pl.svg"
+            <Button title={
+                <div>
+                    <img id='Polish' src="https://flagcdn.com/pl.svg"
                     width="80"
                     alt="Polish" />
-                <p>Polish</p>
-            </a>
-            
-            <a href="/list/Portuguese">
-                <img src="https://flagcdn.com/pt.svg"
+                    <p id='Polish'>Polish</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Polish' >
+            </Button>
+
+            <Button title={
+                <div>
+                    <img id='Portuguese' src="https://flagcdn.com/pt.svg"
                     width="80"
                     alt="Portuguese" />
-                <p>Portuguese</p>
-            </a>
-            
-            <a href="/list/Russian">
-                <img src="https://flagcdn.com/ru.svg"
+                    <p id='Portuguese'>Portuguese</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Portuguese' >
+            </Button>
+
+            <Button title={
+                <div>
+                    <img id='Russian' src="https://flagcdn.com/ru.svg"
                     width="80"
                     alt="Russian" />
-                <p>Russian</p>
-            </a>
-            
-            <a href="/list/Spanish">
-                <img src="https://flagcdn.com/es.svg"
+                    <p id='Russian'>Russian</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Russian' >
+            </Button>
+
+            <Button title={
+                <div>
+                    <img id='Spanish' src="https://flagcdn.com/es.svg"
                     width="80"
                     alt="Spanish" />
-                <p>Spanish</p>
-            </a>
+                    <p id='Spanish'>Spanish</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Spanish' >
+            </Button>
 
-            <a href="/list/Thai">
-                <img src="https://flagcdn.com/th.svg"
+            <Button title={
+                <div>
+                    <img id='Thai' src="https://flagcdn.com/th.svg"
                     width="80"
                     alt="Thai" />
-                <p>Thai</p>
-            </a>
+                    <p id='Thai'>Thai</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Thai' >
+            </Button>
 
-            <a href="/list/Tunisian">
-                <img src="https://flagcdn.com/tn.svg"
+            <Button title={
+                <div>
+                    <img id='Tunisian' src="https://flagcdn.com/tn.svg"
                     width="80"
                     alt="Tunisian" />
-                <p>Tunisian</p>
-            </a>
+                    <p id='Tunisian'>Tunisian</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Tunisian' >
+            </Button>
 
-            <a href="/list/Turkish">
-                <img src="https://flagcdn.com/tr.svg"
+            <Button title={
+                <div>
+                    <img id='Turkish' src="https://flagcdn.com/tr.svg"
                     width="80"
                     alt="Turkish" />
-                <p>Turkish</p>
-            </a>
+                    <p id='Turkish'>Turkish</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Turkish' >
+            </Button>
 
-            <a href="/list/Vietnamese">
-                <img src="https://flagcdn.com/vn.svg"
+            <Button title={
+                <div>
+                    <img id='Vietnamese' src="https://flagcdn.com/vn.svg"
                     width="80"
                     alt="Vietnamese" />
-                <p>Vietnamese</p>
-            </a>
+                    <p id='Vietnamese'>Vietnamese</p>
+                </div>
+            } onClick={(e) => {clickHandle(e)}} id='Vietnamese' >
+            </Button>
 
         </div>
     )
