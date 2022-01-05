54ec85072f5ec2951f511f9cefa4c8412afaf4d3
MFaqihRidho <faqih.fr5@gmail.com>
2022-01-05 10:08:48 +0700

    Menghilangkan Ganteng di footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,7 +3,7 @@ import './style.css'
 function Footer(){
     return(
         <footer>
-            <p>Copyright 2022 Created by <a href="https://github.com/MFaqihRidho">M Faqih Ridho Ganteng</a> Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
+            <p>Copyright 2022 Created by <a href="https://github.com/MFaqihRidho">M Faqih Ridho</a> Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
         </footer>
     )
 }
