633ae93da444f62384835db2a027942222d385c0
MFaqihRidho <faqih.fr5@gmail.com>
2022-01-05 10:25:11 +0700

    MEnambahkan Loading

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,6 +4,7 @@ function Footer(){
     return(
         <footer>
             <p>Copyright 2022 Created by <a href="https://github.com/MFaqihRidho">M Faqih Ridho</a> Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
+            <p>Loading......</p>
         </footer>
     )
 }
